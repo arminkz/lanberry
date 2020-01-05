66e8fb008660b203e9d6db05f7071ad161669196
Armin Kazemi <arminkz@live.com>
2020-01-05 22:50:15 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lantern for Rapsberry Pi
+Build Lantern for armhf devices like Raspberry Pi
+
+![Alt text](/../master/banner.png?raw=true "Screenshot")
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# build script for lantern-armhf
+# ---------------------------------
+# github.com/getlantern/lantern
+# github.com/arminkz/lantern-armhf
+
